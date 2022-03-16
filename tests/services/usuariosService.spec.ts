dd0f3beac36bebfac456a41df7662bf57b0c3432
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2022-03-16 17:23:12 -0300

    test: add tests usuariosService

--- tests/services/usuariosService.spec.ts
+++ tests/services/usuariosService.spec.ts
@@ -14,13 +14,21 @@ const usuario = [{
 
 jest.mock('@/repository/usuarios/usuariosRepository', () => {
   return {
-    find: jest.fn()
+    find: jest.fn(),
+    findById: jest.fn(),
+    create: jest.fn(),
+    findByIdAndUpdate: jest.fn(),
+    findByIdAndRemove: jest.fn()
   }
 })
 
 describe('UsuariosService', () => {
   beforeEach(() => {
     jest.spyOn(UsuariosRepository, 'find').mockReturnValue(Promise.resolve(usuario) as any)
+    jest.spyOn(UsuariosRepository, 'findById').mockReturnValue(Promise.resolve(usuario) as any)
+    jest.spyOn(UsuariosRepository, 'create').mockReturnValue(Promise.resolve(usuario) as any)
+    jest.spyOn(UsuariosRepository, 'findByIdAndUpdate').mockReturnValue(Promise.resolve(usuario) as any)
+    jest.spyOn(UsuariosRepository, 'findByIdAndRemove').mockReturnValue(Promise.resolve([]) as any)
   })
   it('Testando chamada UsuariosService', () => {
     expect(UsuariosService).toBeDefined()
@@ -28,15 +36,26 @@ describe('UsuariosService', () => {
 
   it('Testando chamada UsuariosService.get', async () => {
     jest.spyOn(UsuariosService, 'get').mockReturnValueOnce(Promise.resolve(null))
-    const result = await UsuariosService.get()
-    expect(result).toBeNull()
+    expect(await UsuariosService.get()).toBeNull()
   })
 
   it('Testando chamada UsuariosService.get com retorno objeto', async () => {
-    //    jest.spyOn(UsuariosService, 'get').mockReturnValue(Promise.resolve(usuario) as any)
-    // jest.spyOn(UsuariosRepository, 'find').mockReturnValue(Promise.resolve(usuario) as any)
+    expect(await UsuariosService.get()).toBe(usuario)
+  })
 
-    const result = await UsuariosService.get()
-    expect(result).toBe(usuario)
+  it('Testando chamada UsuariosService.getById com retorno objeto', async () => {
+    expect(await UsuariosService.getById('any_id')).toBe(usuario)
+  })
+
+  it('Testando chamada UsuariosService.create com retorno objeto', async () => {
+    expect(await UsuariosService.create(usuario)).toBe(usuario)
+  })
+
+  it('Testando chamada UsuariosService.update com retorno objeto', async () => {
+    expect(await UsuariosService.update('any_id', usuario)).toBe(usuario)
+  })
+
+  it('Testando chamada UsuariosService.delete com retorno objeto', async () => {
+    expect(await UsuariosService.delete('any_id')).toMatchObject([])
   })
 })
