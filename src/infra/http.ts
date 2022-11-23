a0cc306abb5241a1b79dcc5c9ae5cd7f48ad498e
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2022-11-23 16:23:44 -0300

    test: call api marvel created method get

--- src/infra/http.ts
+++ src/infra/http.ts
@@ -1,11 +1,20 @@
 import axios from 'axios'
+import md5 from 'md5'
 
 export class HttpClient {
-  async get (): Promise<any> {
-    return {
-      statusCode: 200,
-      data: { results: [{ name: 'Hulk' }] }
-    }
+  async getAllCharacters (): Promise<any> {
+    const timestamp = new Date().getTime()
+
+    const privateKey = '0126a49f00e1063a8f441de67dfd56090cb24be6'
+    const apiKey = 'f2aef569ee6f38276c60b9264d1dec2b'
+
+    const hash = md5(timestamp + privateKey + apiKey)
+
+    const url = `https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hash}`
+
+    const result = await axios.get(url)
+    console.log(result.data)
+    return result.data
   }
 }
 export default { HttpClient }
--- tests/infra/http.spec.ts
+++ tests/infra/http.spec.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@/infra/http'
 
 describe('Request API Marvel', () => {
+  let httpClient: HttpClient
+  beforeEach(() => {
+    httpClient = new HttpClient()
+  })
+
   it('Testa chamadas API Marvel', async () => {
-    const httpClient = new HttpClient()
-    const response = await httpClient.get()
-    expect(httpClient.get()).toBeDefined()
-    expect(response.statusCode).toBe(200)
-    expect(response.data.results[0].name).toBe('Hulk')
+    const response = await httpClient.getAllCharacters()
+    expect(response.code).toBe(200)
   })
 })
