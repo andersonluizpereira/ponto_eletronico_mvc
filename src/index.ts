aca07271665356e7e2bb48dbca372c23e374608e
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2022-12-20 16:53:09 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import App from '@/main/app'
 const port = 3000
 
 App.app.listen(port, function () {
-  console.log(`servidor executando na porta ${port}`)
+  console.log(`servidor executandos na porta ${port}`)
 })
